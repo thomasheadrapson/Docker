ace052a790b05845cb109484c0f5aa797dbad7e5
thomasheadrapson <thomasheadrapson@gmail.com>
2024-09-30 08:34:58 +0000

    examen 0.1

---
+++ examen/status/status_test.py
@@ -0,0 +1,56 @@
+import os
+from typing import Dict
+import requests
+
+# adresse de l'API
+address = 'localhost'
+
+# port de l'API
+port = 8000
+
+# arguments
+arguments : Dict[str,str] = dict()
+        
+# route
+route = "status"
+
+# expected status code
+expected_status_code = 200
+
+
+# requête
+r = requests.get(
+    url = f'http://{address}:{port}/{route}',
+    params = arguments
+    )
+
+output = '''
+============================
+    Authentication test
+============================
+
+request done at "/{route}"
+{arguments}
+
+expected result = {expected_status_code}
+actual restult = {status_code}
+
+==>  {test_status}
+
+'''
+
+
+# statut de la requête
+status_code = r.status_code
+
+# affichage des résultats
+if status_code == expected_status_code:
+    test_status = 'SUCCESS'
+else:
+    test_status = 'FAILURE'
+print(output.format(status_code = status_code, expected_status_code = expected_status_code, test_status = test_status, route = route, arguments = arguments))
+
+# impression dans un fichier
+if os.environ.get('LOG') == 1:
+    with open('api_test.log', 'a') as file:
+        file.write(output)
---
+++ examen/status/status_test_v2.py
@@ -0,0 +1,16 @@
+import os
+import requests
+
+
+address = "localhost"
+
+port = "8000"
+
+def make_request(address: "str", port: "int", params: "dict[str, str]", ):
+    return True
+
+def test_status() -> None:
+    url = f"http://{address}:{port}/status"
+    r = request.get(url = url)
+    assert (r.) == 1
+
